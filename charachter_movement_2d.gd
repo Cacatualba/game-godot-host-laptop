921e12896816e87e8e53bdf837d037dcfa75c815
Cacatualba <dragonbarbu2.0@gmail.com>
2025-12-03 16:04:22 -0500

    working character + ground

--- charachter_movement_2d.gd
+++ charachter_movement_2d.gd
@@ -1,46 +1,26 @@
 extends CharacterBody2D
 
 @export var speed := 200.0
+@export var jump_force := 400.0
+@export var gravity := 800.0
 
-func _physics_process(_delta):
-	var direction = Vector2.ZERO
+func _physics_process(delta):
+	var direction = 0
 
-	var r = Input.is_action_pressed("ui_right")
-	var r2 = Input.is_action_pressed("ui_d")
-	var l = Input.is_action_pressed("ui_left")
-	var l2 = Input.is_action_pressed("ui_a")
-	var d = Input.is_action_pressed("ui_down")
-	var d2 = Input.is_action_pressed("ui_s")
-	var u = Input.is_action_pressed("ui_up")
-	var u2 = Input.is_action_pressed("ui_w")
+	# Horizontal input
+	if Input.is_action_pressed("ui_left"):
+		direction -= 1
+	if Input.is_action_pressed("ui_right"):
+		direction += 1
 
-	# debug input
-	if r or r2 or l or l2 or d or d2 or u or u2:
-		print("input:", "R" if r else "-","R2" if r2 else "-", " L" if l else "-",  " L2" if l2 else "-"," D" if d else "-"," D2" if d2 else "-", " U" if u else "-", " U2" if u2 else "-")
+	velocity.x = direction * speed
 
-	if r:
-		direction.x += 1
-	if r2:
-		direction.x += 1
-	if l:
-		direction.x -= 1
-	if l2:
-		direction.x -= 1
-	if d:
-		direction.y += 1
-	if d2:
-		direction.y += 1
-	if u:
-		direction.y -= 1
-	if u2:
-		direction.y -= 1
+	# Apply gravity manually
+	velocity.y += gravity * delta
 
-	if direction != Vector2.ZERO:
-		direction = direction.normalized()
+	# Jump
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = -jump_force
 
-	velocity = direction * speed
-
-	# debug velocity
-	print("velocity:", velocity)
-
+	# Move the character
 	move_and_slide()
